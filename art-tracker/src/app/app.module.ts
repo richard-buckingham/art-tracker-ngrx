8fb5ef67176678a3793ea11a38c11d35ca893884
richie.buckingham <richie.buckingham@gmail.com>
2018-06-25 13:30:11 +1000

    put app component within container folder

--- art-tracker/src/app/app.module.ts
+++ art-tracker/src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./containers/app/app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+/* <script src="https://www.gstatic.com/firebasejs/5.1.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBp3Uu2srg3im1bbA5M88k-gfTfBqqbuQ0",
+    authDomain: "art-tracker-ngrx.firebaseapp.com",
+    databaseURL: "https://art-tracker-ngrx.firebaseio.com",
+    projectId: "art-tracker-ngrx",
+    storageBucket: "art-tracker-ngrx.appspot.com",
+    messagingSenderId: "865746449336"
+  };
+  firebase.initializeApp(config);
+</script>
+ */
